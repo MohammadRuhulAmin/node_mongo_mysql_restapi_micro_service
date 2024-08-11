79aea5c33e63b5e41c28d0431d997c75d90d96bb
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-08-11 09:03:20 +0600

    post operation established

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,28 @@
-export const fetch = async (req,res)=>{
-  try{
-    return res.json("hellow world");
-  }
-  catch(error){
-    res.status(500).json({error: "Internal server error."});
+import User from "../model/userModel.js";
+
+export const create = async (req, res) => {
+  try {
+    const userData = new User(req.body);
+    const { email } = userData;
+    console.log("running");
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const savedUser = await userData.save();
+    return res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server error." });
   }
 };
+
+export const fetch = async (req, res) => {
+  try {
+    return res.json("Hello world");
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
